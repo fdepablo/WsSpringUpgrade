4b67fd830370715febd7812c1a80026f0d04b861
Felix de Pablo <f.depablo.lobo@gmail.com>
2025-01-09 17:26:12 +0100

    autowired

---
+++ 01_SpringXML/src/main/java/main/Main05.java
@@ -0,0 +1,21 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import modelo.entidad.Director;
+import modelo.entidad.Pelicula;
+import modelo.entidad.Persona;
+
+public class Main05 {
+
+	public static ApplicationContext context;
+	
+	public static void main(String[] args) {
+		context = new ClassPathXmlApplicationContext("context05.xml");
+
+		Pelicula p = context.getBean("pelicula",Pelicula.class);
+		System.out.println(p);
+	}
+
+}
--- 01_SpringXML/src/main/java/modelo/entidad/Pelicula.java
+++ 01_SpringXML/src/main/java/modelo/entidad/Pelicula.java
@@ -3,6 +3,7 @@ package modelo.entidad;
 public class Pelicula {
 	private String titulo;
 	private Director director;
+	private Persona productor;
 	
 	public String getTitulo() {
 		return titulo;
@@ -17,10 +18,18 @@ public class Pelicula {
 		this.director = director;
 	}
 	
+	public Persona getProductor() {
+		return productor;
+	}
+	public void setProductor(Persona productor) {
+		this.productor = productor;
+	}
 	@Override
 	public String toString() {
-		return "Pelicula [titulo=" + titulo + ", director=" + director + "]";
+		return "Pelicula [titulo=" + titulo + ", director=" + director + ", productor=" + productor + "]";
 	}
+	
+	
 	
 	
 }
